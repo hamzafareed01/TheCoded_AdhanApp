72e0dbe815f67cca6a3ea2e650d77272ec82b330
Hamza Fareed <86845085+hamzafareed01@users.noreply.github.com>
2026-03-26 23:27:39 -0500

    re triggering deploy

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use("/audio", express.static(path.join(__dirname, "audio"), { maxAge: "1h" }
 // -----------------------------
 // Constants
 // -----------------------------
-
+// re-trigger deploy for update....delete later
 const PRAYERS = ["fajr", "dhuhr", "asr", "maghrib", "isha"];
 const AMAZON_TOKEN_CACHE_TTL_MS = 5 * 60 * 1000;
 const GOOGLE_PLACES_BASE = "https://places.googleapis.com/v1";
