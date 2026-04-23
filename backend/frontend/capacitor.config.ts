4554f8518f226d525f46c177be07490b957bc7b5
Hamza Fareed <86845085+hamzafareed01@users.noreply.github.com>
2026-04-23 16:18:04 -0500

    no comment

--- backend/frontend/capacitor.config.ts
+++ backend/frontend/capacitor.config.ts
@@ -1,27 +1,11 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
-const PROD_WEB_URL = "https://nice-ground-009684610.1.azurestaticapps.net";
-
 const config: CapacitorConfig = {
   appId: "com.thecoded.adhanhome",
   appName: "AdhanCast",
-  webDir: "dist",
+  webDir: "build",
   server: {
-    url: PROD_WEB_URL,
-    cleartext: false,
-    allowNavigation: [
-      "nice-ground-009684610.1.azurestaticapps.net",
-      "nice-ground-009684610-1.centralus.1.azurestaticapps.net",
-      "app-adhanhome-api-prod-cdfdcsfeb5gtd7e9.centralus-01.azurewebsites.net",
-      "*.amazon.com",
-      "www.amazon.com",
-      "na.account.amazon.com",
-      "api.amazon.com",
-      "assets.loginwithamazon.com",
-      "pitangui.amazon.com",
-      "layla.amazon.com",
-      "alexa.amazon.co.jp"
-    ]
+    androidScheme: "https"
   }
 };
 
